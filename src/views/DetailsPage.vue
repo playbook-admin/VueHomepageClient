50e7f06cb64fcadbc01ba43f9b65f0aecc498e44
Gunnar Siréus <gunnar.sireus@gmail.com>
2024-08-29 13:40:21 +0200

    all there

--- src/views/DetailsPage.vue
+++ src/views/DetailsPage.vue
@@ -90,7 +90,7 @@
 </template>
 
 <script>
-//import PhotoFrame from '../photos/PhotoFrame.vue';
+import PhotoFrame from '../photos/PhotoFrame.vue';
 import { ref, onMounted, computed } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import * as apiClient from '../helpers/ApiHelpers';
@@ -99,7 +99,7 @@ import { useApiAddress } from '../components/useGlobalState';
 export default {
   name: "DetailsPage", // Corrected this line
   components: {
-    //PhotoFrame
+    PhotoFrame
   },
   setup() {
     const route = useRoute();
